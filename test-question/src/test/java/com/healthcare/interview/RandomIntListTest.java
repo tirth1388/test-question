02ced2e4211d2566fa46af21d16e0def9a9b3f55
tirth1388 <41162920+tirth1388@users.noreply.github.com>
2018-07-13 12:37:56 -0400

    updated unit test for ordered intersection

--- test-question/src/test/java/com/healthcare/interview/RandomIntListTest.java
+++ test-question/src/test/java/com/healthcare/interview/RandomIntListTest.java
@@ -1,23 +1,23 @@
 package com.healthcare.interview;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static com.healthcare.interview.RandomIntListUtil.*;
+import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
-
 public class RandomIntListTest {
 	
 	List<Integer> randomIntList;
 	
 	@Before
 	public void init() {
-		randomIntList = RandomIntListUtil.generateRandIntList();
+		randomIntList = generateRandIntList();
 	}
 	
 	@Test
@@ -34,7 +34,34 @@ public class RandomIntListTest {
 	@Test
 	public void containsDuplicateTest() {
 		Set<Integer> randomIntSet = new HashSet<>(randomIntList);
-		assertSame(randomIntSet.size()!=randomIntList.size(), RandomIntListUtil.containsDuplicate(randomIntList));
-	}	
-
+		assertSame(randomIntSet.size()!=randomIntList.size(), containsDuplicate(randomIntList));
+	}
+	
+	@Test
+	public void reduceToDuplicatesTest() {
+		List<Integer> testList = Arrays.asList(11, 12, 13, 13, 14, 14, 15, 15, 16); //two of each for 13, 14 and 15
+		Set<Integer> expectedSet = new HashSet<>(Arrays.asList(13, 14, 15));  //expected result
+		Set<Integer> resultSet = new HashSet<>(reduceToDuplicates(testList)); //get the result set
+		
+		assertEquals(expectedSet, resultSet);		
+	}
+	
+	@Test
+	public void findOrderedIntersectionTest() {
+		List<Integer> testList1 = Arrays.asList(11, 12, 13, 13, 14, 14, 15, 15, 16); 
+		List<Integer> testList2 = Arrays.asList( 16, 12, 15, 120, 121, 222, 205, 105, 107, 2, 1); // 12, 15, 16 is intersection
+		
+		List<Integer> expectedIntersection = Arrays.asList(15, 12, 16);  //expected result
+		Collections.sort(expectedIntersection); //sort the expected result
+		List<Integer> resultedSortedList = findOrderedIntersection(testList1, testList2); //get the result set
+		
+		//test
+		//first assert they contain the same elements and same length
+		assertEquals(expectedIntersection, resultedSortedList); 
+		
+		//assert they are ordered too
+		for(int i = 0; i <expectedIntersection.size(); i++) {
+			assertEquals(expectedIntersection.get(i), resultedSortedList.get(i)); 
+		}	
+	}
 }
